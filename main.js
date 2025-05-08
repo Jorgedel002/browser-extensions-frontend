f824386a4a15f8f78cbb55ad60fe7a7d4e0c3814
Jorgedel002 <jorgedel002@gmail.com>
2025-05-08 17:43:29 -0300

    Agrego la seccion de filtros con funcionalidad de cambio de estilos entre filtros(active), tanto light como dark mode y responsive

--- main.js
+++ main.js
@@ -3,10 +3,45 @@ document.addEventListener("DOMContentLoaded", () => {
     const darkModeButton = document.getElementById("darkModeButton");
     const darkModeIcon = document.getElementById("darkModeIcon");
     const logo = document.getElementById("logo");
+    const filterAll = document.getElementById("filterAll");
+    const filterActive = document.getElementById("filterActive");
+    const filterInactive = document.getElementById("filterInactive");
+    const filterButtons = document.querySelectorAll(".filter-btn");
     
     darkModeButton.addEventListener("click", () => {
         htmlContent.classList.toggle("dark");
         darkModeIcon.src = htmlContent.classList.contains("dark") ? "assets/img/icon-sun.svg" : "assets/img/icon-moon.svg";
         logo.style.stroke = htmlContent.classList.contains("dark") ? "#EBF2FC" : "#040918";
+    });
+
+    filterButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            filterButtons.forEach(btn => btn.classList.remove("active"));
+            button.classList.add("active");
+        });
+    });
+
+    function filterAllFunction(){
+        
+    }
+
+    function filterActiveFunction(){
+        
+    }
+
+    function filterInactiveFunction(){
+        
+    }
+
+    filterAll.addEventListener("click", () => {
+       
+    });
+
+    filterActive.addEventListener("click", () => {
+        
+    });
+
+    filterInactive.addEventListener("click", () => {
+        
     });
 });
