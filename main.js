75caebf25d5d9b9d767cbe9432a3151dd8d52dd4
Jorgedel002 <jorgedel002@gmail.com>
2025-05-08 02:03:27 -0300

    Desarrollo la funcion de dark mode. al dar click en el icono moon/sun, cambia entre light y dark

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     const htmlContent = document.getElementById("htmlContent");
+    const darkModeButton = document.getElementById("darkModeButton");
+    const darkModeIcon = document.getElementById("darkModeIcon");
+    const logo = document.getElementById("logo");
     
+    darkModeButton.addEventListener("click", () => {
+        htmlContent.classList.toggle("dark");
+        darkModeIcon.src = htmlContent.classList.contains("dark") ? "assets/img/icon-sun.svg" : "assets/img/icon-moon.svg";
+        logo.style.stroke = htmlContent.classList.contains("dark") ? "#EBF2FC" : "#040918";
+    });
 });
