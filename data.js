b05b85ce6ec9691e622683d73637cb0847875462
Jorgedel002 <jorgedel002@gmail.com>
2025-05-12 11:21:04 -0300

    Agrego la funcionalidad de filter all, inactive y active

--- data.js
+++ data.js
@@ -4,71 +4,95 @@ let data = [
         description: "Quickly inspect page layouts and visualize element boundaries.",
         logo: "assets/img/logo-devlens.svg",
         altLogo: "logo-devlens",
+        id: 1,
+        active: false
     },
     {
         name: "StyleSpy",
         description: "Instantly analye and copy CSS from any webpage element.",
         logo: "assets/img/logo-style-spy.svg",
         altLogo: "logo-style-spy",
+        id: 2,
+        active: false
     },
     {
         name: "SpeedBoost",
         description: "Optimizes browser resource usage to accelerate page loading.",
         logo: "assets/img/logo-speed-boost.svg",
         altLogo: "logo-speed-boost",
+        id: 3,
+        active: false
     },
     {
         name: "JSONWizard",
         description: "Formats, validates, and prettifies JSON responses in-browser.",
         logo: "assets/img/logo-json-wizard.svg",
         altLogo: "logo-json-wizard",
+        id: 4,
+        active: false
     },
     {
         name: "TabMaster Pro",
         description: "Organize browser tabs into groups and sessions.",
-        logo: "assets/img/logo-tabmaster-pro.svg",
-        altLogo: "logo-tabmaster-pro",
+        logo: "assets/img/logo-tab-master-pro.svg",
+        altLogo: "logo-tab-master-pro",
+        id: 5,
+        active: false
     },
     {
         name: "ViewportBuddy",
         description: "Simulates various screen resolutions directly within the browser.",
         logo: "assets/img/logo-viewport-buddy.svg",
         altLogo: "logo-viewport-buddy",
+        id: 6,
+        active: false
     },
     {
         name: "Markup Notes",
         description: "Enables annotation and notes directly onto webpages for collaborative debugging.",
         logo: "assets/img/logo-markup-notes.svg",
         altLogo: "logo-markup-notes",
+        id: 7,
+        active: false
     },
     {
         name: "GridGuides",
         description: "Overlay customizable grids and alignment guides on any webpage.",
         logo: "assets/img/logo-grid-guides.svg",
         altLogo: "logo-grid-guides",
+        id: 8,
+        active: false
     },
     {
         name: "Palette Picker",
         description: "Instantly extracts color palettes from any webpage.",
         logo: "assets/img/logo-palette-picker.svg",
         altLogo: "logo-palette-picker",
+        id: 9,
+        active: false
     },
     {
         name: "LinkChecker",
         description: "Scans and highlights broken links on any page.",
         logo: "assets/img/logo-link-checker.svg",
         altLogo: "logo-link-checker",
+        id: 10,
+        active: false
     },
     {
         name: "DOM Snapshot",
         description: "Capture and export DOM structures quickly.",
         logo: "assets/img/logo-dom-snapshot.svg",
         altLogo: "logo-dom-snapshot",
+        id: 11,
+        active: false
     },
     {
         name: "ConsolePlus",
         description: "Enhance developer console with advanced filtering and logging. ",
         logo: "assets/img/logo-console-plus.svg",
         altLogo: "logo-console-plus",
+        id: 12,
+        active: false
     }
 ]
--- main.js
+++ main.js
@@ -7,10 +7,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const filterActive = document.getElementById("filterActive");
     const filterInactive = document.getElementById("filterInactive");
     const filterButtons = document.querySelectorAll(".filter-btn");
-    const chkBox = document.getElementById("chk-box");
-    const toggle = document.getElementById("toggler");
-    let slider = document.getElementById("slider");
-    
+    const extensionContainer = document.getElementById("extensionContainer");
+
     darkModeButton.addEventListener("click", () => {
         htmlContent.classList.toggle("dark");
         darkModeIcon.src = htmlContent.classList.contains("dark") ? "assets/img/icon-sun.svg" : "assets/img/icon-moon.svg";
@@ -20,51 +18,99 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
+    data.forEach(extension => {
+        const extensionCard = document.createElement("div");
+        extensionCard.id = extension.name;
+        extensionCard.classList.add("flex", "flex-col", "justify-between", "bg-neutral-0", "dark:bg-neutral-800", "shadow-sm", "rounded-xl", "p-4", "border-1", "border-neutral-200", "dark:border-neutral-600", "min-h-[184px]", "3sm:min-h-[272px]", "2sm:min-h-[204px]", "md:min-h-[254px]");
+        extensionCard.innerHTML = `
+            <div class="flex gap-3 items-start">
+                <img src="${extension.logo}" alt="${extension.altLogo}">
+                <div>
+                    <h2 class="font-bold text-lg text-neutral-900 dark:text-neutral-100 mb-1">${extension.name}</h2>
+                    <p class="text-neutral-600 dark:text-neutral-400 text-sm md:text-base">${extension.description}</p>
+                </div>
+            </div>
+            <div class="flex items-center justify-between mt-5 md:mt-12">
+                <button class="border-1 border-stone-400 font-semibold outline-none cursor-pointer py-2 px-4 text-sm rounded-full text-neutral-900 dark:text-neutral-0" id="removeBtn-${extension.id}">Remove</button>
+                <label for="" class='relative w-11 h-6 inline-flex'>
+                    <input type="checkbox" id="chk-box-${extension.id}" class='w-full h-full opacity-0' />
+                    <span id='toggler-${extension.id}' class="absolute w-full h-full bg-stone-300 rounded-full left-0 top-0 cursor-pointer transition-all duration-500 linear dark:bg-neutral-600">
+                        <span class="relative w-full h-full top-0 left-0 bottom-0">
+                        <span id='slider-${extension.id}' class='absolute w-[19px] h-[19px] bg-white rounded-full left-1 right-1 top-[2px] bottom-1 cursor-pointer transition-all duration-500 linear'></span>
+                        </span>
+                    </span>
+                </label>
+            </div>
+        `;
+        extensionContainer.appendChild(extensionCard);
+        
+        const chkBox = document.getElementById(`chk-box-${extension.id}`);
+        const toggle = document.getElementById(`toggler-${extension.id}`);
+        const slider = document.getElementById(`slider-${extension.id}`);
+        const removeBtn = document.getElementById(`removeBtn-${extension.id}`);
+
+        if(extension.active){
+            chkBox.checked = true;
+            slider.style.transform = "translateX(18px)";
+            toggle.style.backgroundColor = "#de473f";
+            slider.style.backgroundColor = "white";
+        }
+        
+        toggle.addEventListener("click", () => {
+            chkBox.checked = !chkBox.checked;
+            if (chkBox.checked) {
+                slider.style.transform = "translateX(18px)";
+                toggle.style.backgroundColor = "#de473f";
+                slider.style.backgroundColor = "white";
+                extension.active = true;
+            } else {
+                slider.style.transform = "translateX(0px)";
+                if(htmlContent.classList.contains('dark')){
+                    toggle.style.backgroundColor = "#545969";
+                }else{
+                    toggle.style.backgroundColor = "#d6d3d1";
+                }
+                slider.style.backgroundColor = "white";
+                extension.active = false;
+            }
+        });
+        
+        removeBtn.addEventListener("click", () => {
+            extensionCard.remove();
+        });
+    });
+
+    filterAll.addEventListener("click", () => {
+        data.forEach(extension => {
+            const extensionCard = document.getElementById(extension.name);
+            if (extensionCard) {
+                extensionCard.style.display = "flex";
+            }
+        });
+    });
+
+    filterActive.addEventListener("click", () => {
+        data.forEach(extension => {
+            const extensionCard = document.getElementById(extension.name);
+            if (extensionCard) {
+                extensionCard.style.display = extension.active ? "flex" : "none";
+            }
+        });
+    });
+
+    filterInactive.addEventListener("click", () => {
+        data.forEach(extension => {
+            const extensionCard = document.getElementById(extension.name);
+            if (extensionCard) {
+                extensionCard.style.display = extension.active ? "none" : "flex";
+            }
+        });
+    });
+
     filterButtons.forEach(button => {
         button.addEventListener("click", () => {
             filterButtons.forEach(btn => btn.classList.remove("active"));
             button.classList.add("active");
         });
-    });
-
-    toggle.addEventListener("click", () => {
-    chkBox.checked = !chkBox.checked;
-    if (chkBox.checked) {
-        slider.style.transform = "translateX(18px)";
-        toggle.style.backgroundColor = "#de473f";
-        slider.style.backgroundColor = "white";
-    } else {
-        slider.style.transform = "translateX(0px)";
-        if(htmlContent.classList.contains('dark')){
-            toggle.style.backgroundColor = "#545969";
-        }else{
-            toggle.style.backgroundColor = "#d6d3d1";
-        }
-        slider.style.backgroundColor = "white";  
-    }
-    });
-
-    function filterAllFunction(){
-        
-    }
-
-    function filterActiveFunction(){
-        
-    }
-
-    function filterInactiveFunction(){
-        
-    }
-
-    filterAll.addEventListener("click", () => {
-       
-    });
-
-    filterActive.addEventListener("click", () => {
-        
-    });
-
-    filterInactive.addEventListener("click", () => {
-        
     });
 });
